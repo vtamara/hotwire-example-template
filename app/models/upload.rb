2760634c5d0bb61d0d06e65a0c71324450f42564
Sean Doyle <sean.p.doyle24@gmail.com>
2021-05-05 00:20:21 -0400

    broadcast updates when touched

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,5 @@
 class Upload < ApplicationRecord
   has_one_attached :file
+
+  after_touch -> { broadcast_replace_later if file.analyzed? }
 end
--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,1 +1,8 @@
 Rails.application.config.active_storage.analyzers.prepend SlowImageAnalyzer
+
+ActiveSupport.on_load :active_storage_blob do
+  def analyze
+    super
+    attachments.each(&:touch)
+  end
+end
