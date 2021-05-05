79f186c8d4d508d104f78ce36b108c763bd17661
Sean Doyle <sean.p.doyle24@gmail.com>
2021-05-04 23:59:05 -0400

    Upload show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :uploads, only: [:new, :create]
+  resources :uploads, only: [:new, :create, :show]
 end
